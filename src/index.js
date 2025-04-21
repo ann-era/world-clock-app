86a5ba50cbcef7b1cff6201bbb5db00fdd27a872
ann-era <148546178+ann-era@users.noreply.github.com>
2025-04-21 17:38:15 +0800

    IImproved time functionality
    - Added seconds
    - Auto refresh time at every second
    - Simplify code
    - Update font size

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
+function updateAllTimes() {
+  cities.forEach((city) => updateTime(city.id, city.zone));
+}
+
+function updateTime(id, timezone) {
+  let city = document.querySelector(`#${id}`);
+  if (city) {
+    let cityTime = city.querySelector(".city-time");
+    let cityDate = city.querySelector(".city-date");
+    let time = moment().tz(timezone);
+    cityTime.innerHTML = time.format("h:mm:ss A");
+    cityDate.innerHTML = time.format("DD MMM YYYY");
+  }
+}
+
 function changeTheme() {
   let updateTheme = document.querySelectorAll(".theme-target");
   for (let i = 0; i < updateTheme.length; i++) {
@@ -8,42 +23,12 @@ function changeTheme() {
 let themeButton = document.querySelector(".theme");
 themeButton.addEventListener("click", changeTheme);
 
-//los angeles
-let losAngeles = document.querySelector("#los-angeles");
-if (losAngeles) {
-  let losAngelesTime = losAngeles.querySelector(".city-time");
-  let losAngelesDate = losAngeles.querySelector(".city-date");
-  let losAngelesTimeZone = moment().tz("America/Los_Angeles");
-  losAngelesTime.innerHTML = losAngelesTimeZone.format("hh:mm A");
-  losAngelesDate.innerHTML = losAngelesTimeZone.format("DD MMM YYYY");
-}
+const cities = [
+  { id: "los-angeles", zone: "America/Los_Angeles" },
+  { id: "sydney", zone: "Australia/Sydney" },
+  { id: "tokyo", zone: "Asia/Tokyo" },
+  { id: "paris", zone: "Europe/Paris" },
+];
 
-//sydney
-let sydney = document.querySelector("#sydney");
-if (sydney) {
-  let sydneyTime = sydney.querySelector(".city-time");
-  let sydneyDate = sydney.querySelector(".city-date");
-  let sydneyTimeZone = moment().tz("Australia/Sydney");
-  sydneyTime.innerHTML = sydneyTimeZone.format("hh:mm A");
-  sydneyDate.innerHTML = sydneyTimeZone.format("DD MMM YYYY");
-}
-
-//tokyo
-let tokyo = document.querySelector("#tokyo");
-if (tokyo) {
-  let tokyoTime = tokyo.querySelector(".city-time");
-  let tokyoDate = tokyo.querySelector(".city-date");
-  let tokyoTimeZone = moment().tz("Asia/Tokyo");
-  tokyoTime.innerHTML = tokyoTimeZone.format("hh:mm A");
-  tokyoDate.innerHTML = tokyoTimeZone.format("DD MMM YYYY");
-}
-
-//paris
-let paris = document.querySelector("#paris");
-if (paris) {
-  let parisTime = paris.querySelector(".city-time");
-  let parisDate = paris.querySelector(".city-date");
-  let parisTimeZone = moment().tz("Europe/Paris");
-  parisTime.innerHTML = parisTimeZone.format("hh:mm A");
-  parisDate.innerHTML = parisTimeZone.format("DD MMM YYYY");
-}
+updateAllTimes();
+setInterval(updateAllTimes, 1000);
