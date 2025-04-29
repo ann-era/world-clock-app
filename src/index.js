e7ab7bfa1a6a516b8a4af5ad981bf293a86f60f0
ann-era <148546178+ann-era@users.noreply.github.com>
2025-04-29 14:57:01 +0800

    updated select city functionality

--- src/index.js
+++ src/index.js
@@ -20,14 +20,79 @@ function changeTheme() {
   }
 }
 
+function changeToCity(event) {
+  let timezone = event.target.value;
+  let displayCity = document.querySelector("#content");
+  if (timezone) {
+    let cityDetails = cities.find((item) => item.zone === timezone);
+    let cityId = cityDetails.id;
+    let cityName = cityDetails.city;
+    displayCity.innerHTML = `
+      <div class="city-card theme-target dark-theme" id="${cityId}">
+        <div class="city-clock">🕒</div>
+        <div class="city-info theme-target dark-theme">
+          ${cityName}
+          <div class="city-time theme-target dark-theme"></div>
+          <div class="city-date"></div>
+        </div>
+      </div>
+      `;
+    updateTime(cityId, timezone);
+  } else {
+    displayCity.innerHTML = `
+      <div class="city-card theme-target dark-theme" id="los-angeles">
+        <div class="city-clock">🕒</div>
+        <div class="city-info theme-target dark-theme">
+          Los Angeles
+          <div class="city-time theme-target dark-theme"></div>
+          <div class="city-date"></div>
+        </div>
+      </div>
+      <div class="city-card theme-target dark-theme" id="sydney">
+        <div class="city-clock">🕒</div>
+        <div class="city-info theme-target dark-theme">
+          Sydney
+          <div class="city-time theme-target dark-theme"></div>
+          <div class="city-date"></div>
+        </div>
+      </div>
+      <div class="city-card theme-target dark-theme" id="tokyo">
+        <div class="city-clock">🕒</div>
+        <div class="city-info theme-target dark-theme">
+          Tokyo
+          <div class="city-time theme-target dark-theme"></div>
+          <div class="city-date"></div>
+        </div>
+      </div>
+      <div class="city-card theme-target dark-theme" id="paris">
+        <div class="city-clock">🕒</div>
+        <div class="city-info theme-target dark-theme">
+          Paris
+          <div class="city-time theme-target dark-theme"></div>
+          <div class="city-date"></div>
+        </div>
+      </div>
+    `;
+    updateAllTimes();
+  }
+  let body = document.querySelector("body");
+  if (!body.classList.contains("dark-theme")) {
+    let elements = document.querySelectorAll(".dark-theme");
+    elements.forEach((el) => el.classList.remove("dark-theme"));
+  }
+}
+
 let themeButton = document.querySelector(".theme");
 themeButton.addEventListener("click", changeTheme);
 
+let citySelect = document.querySelector("#city-select");
+citySelect.addEventListener("change", changeToCity);
+
 const cities = [
-  { id: "los-angeles", zone: "America/Los_Angeles" },
-  { id: "sydney", zone: "Australia/Sydney" },
-  { id: "tokyo", zone: "Asia/Tokyo" },
-  { id: "paris", zone: "Europe/Paris" },
+  { id: "los-angeles", zone: "America/Los_Angeles", city: "Los Angeles" },
+  { id: "sydney", zone: "Australia/Sydney", city: "Sydney" },
+  { id: "tokyo", zone: "Asia/Tokyo", city: "Tokyo" },
+  { id: "paris", zone: "Europe/Paris", city: "Paris" },
 ];
 
 updateAllTimes();
