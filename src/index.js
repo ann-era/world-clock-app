f6394ef8fb51b446c6e77e07513ebcc69e358259
ann-era <148546178+ann-era@users.noreply.github.com>
2025-07-17 15:48:09 +0800

    removed margin bottom for Date

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ function defaultClockCards() {
           <div class="card-body text-center">
             <h5 class="card-title fw-bold text-success mb-3">${city.name}</h5>
             <p class="display-6 mb-2">${time}</p>
-            <p class="text-muted">${date}</p>
+            <p class="text-muted mb-0">${date}</p>
           </div>
         </div>
       </div>
